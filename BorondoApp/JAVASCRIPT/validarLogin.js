5ba57588956903564ef115099de91c8ea07cb3cd
Andres Montoya Castillo <a.montoya0218@gmail.com>
2022-12-03 10:28:31 -0500

    Subida de archivos

---
+++ BorondoApp/JAVASCRIPT/validarLogin.js
@@ -0,0 +1,55 @@
+function ir() {
+
+   
+    let correo = document.getElementById("correo").value;
+    let clave1 = document.getElementById("clave1").value;
+    
+
+    //const boton = document.getElementById("boton");
+     //const formulario = document.getElementById("formulario"); 
+     const parrafo = document.getElementById("mensaje");
+
+    
+    let mensaje = ""
+    let regexEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esValido = regexEmail.test(correo)
+     
+    parrafo.innerHTML="";
+
+
+    if(correo=="" || clave1==""  )
+    {
+        mensaje = "¡hay campos vacios!"
+        entrada=true;
+        parrafo.innerHTML = mensaje
+        return false; 
+    }
+
+   
+    
+    if (clave1.length<=9) {
+        mensaje = "¡Tamaño de la contraseña muy corto!"
+        parrafo.innerHTML = mensaje
+        return false;         
+        
+    }
+    
+   
+
+
+    if(esValido==true){}
+        else{
+        mensaje = "¡Correo invalido!"
+        parrafo.innerHTML = mensaje
+        return false;         
+         }    
+
+
+    function datos(){
+
+        let vercorreo=correo;
+        let verclave1=correo;
+        let verclave2=correo; 
+    }
+
+}
